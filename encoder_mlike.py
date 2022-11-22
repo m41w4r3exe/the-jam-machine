07339c669c940881bf9e9205cc96915eafe3d5a9
m41w4r3.exe <33025255+loopbender@users.noreply.github.com>
2022-11-23 00:17:25 +0100

    partial add N-bar per-instrument for encoder

--- encoder_mlike.py
+++ encoder_mlike.py
@@ -8,8 +8,6 @@ from utils import writeToFile, EventToText
 pitch_range = range(21, 109)
 beat_res = {(0, 400): 8}
 nb_velocities = 5
-generate_by_N_bars = 4
-time_signature = (4, 4)
 
 additional_tokens = {
     "Chord": False,
@@ -32,55 +30,49 @@ tokens = tokenizer.midi_to_tokens(midi)
 
 event_to_text = EventToText()
 
-track_length_in_bars = midi.max_tick / midi.ticks_per_beat / time_signature[0]
-
 piece_encoded_text = "PIECE_START"
 
-tokens_by_instruments_by_N_bars = list(range(len(midi.instruments)))
-for ins, _ in enumerate(midi.instruments):
-    tokens_by_instruments_by_N_bars[ins] = []
+for instrument in midi.instruments:
+    inst_tokens = tokenizer.track_to_tokens(instrument)
+    midi_events = tokenizer.tokens_to_events(inst_tokens)
 
-for inst_index, instrument in enumerate(midi.instruments):
-    piece_encoded_text += f" TRACK_START INST={instrument.program} BAR_START "
-    inst_tokens = tokenizer.track_to_tokens(midi.instruments[inst_index])
-    tokens_events = tokenizer.tokens_to_events(inst_tokens)
+    track_encoded = f"TRACK_START INST={instrument.program} BAR_START "
+    inst_events_in_bars = []
+    bar_count = 0
     beat_count = 0
-    bar_encoded_count = 0
+    for index, event in enumerate(midi_events):
 
-    for event in tokens_events:
         if event.type == "Time-Shift":
             values = list(map(int, event.value.split(".")))
-            beat_count += int(values[0] + (values[1] / 8))
+            beat_count += values[0] + (values[1] / 8)
 
-            # # TODO: Deal with notes clashing with bar finishings here
-            while beat_count > 4:
-                bar_encoded_count += 1
+            # TODO: Deal with notes clashing with bar finishings here
+            while beat_count >= 4:
                 beat_count -= 4
-                if bar_encoded_count == generate_by_N_bars:
+                values[0] = int(beat_count)
+                bar_count += 1
+                if bar_count == 8:
                     break
-                piece_encoded_text += "BAR_END "
-                piece_encoded_text += "BAR_START "
+                track_encoded += "BAR_END BAR_START "
 
-            if beat_count == 4:
-                bar_encoded_count += 1
-                if bar_encoded_count == generate_by_N_bars:
-                    break
-                piece_encoded_text += "BAR_END "
-                piece_encoded_text += "BAR_START "
+            # Update beat count of next timeshift
+            event.value = ".".join(map(str, values))
 
-            if beat_count < 4:
-                event.value = (
-                    str(beat_count) + "." + str(values[1]) + "." + str(values[2])
-                )
+            if bar_count == 8:
+                bar_count = 0
+                track_encoded += event_to_text.string(event)
+                track_encoded += "BAR_END TRACK_END"
+                inst_events_in_bars.append(track_encoded)
+                track_encoded = f"TRACK_START INST={instrument.program} BAR_START "
+                continue
 
-        if beat_count < 4:
-            piece_encoded_text += event_to_text.string(event)
+        track_encoded += event_to_text.string(event)
 
-        if bar_encoded_count == generate_by_N_bars:
-            piece_encoded_text += "BAR_END TRACK_END "
-            tokens_by_instruments_by_N_bars[inst_index].append([piece_encoded_text])
-            piece_encoded_text = f"TRACK_START INST={instrument.program} BAR_START "
-            bar_encoded_count = 0
+    if bar_count != 0:
+        track_encoded += "BAR_END BAR_START" * (8 - bar_count)
+        track_encoded += "BAR_END TRACK_END"
+        inst_events_in_bars.append(track_encoded)
+    print(inst_events_in_bars)
 
-# piece_encoded_text = "PIECE_END"
+
 # writeToFile(f"./midi/{midi_filename}_text_mlike.txt", piece_encoded_text)
