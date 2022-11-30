5e24cf3f0b707418c0208421b253cf2ea1c4182d
Jean Simonnet <jeanasimonnet@gmail.com>
2022-11-30 22:30:55 +0100

    fixing bugs in generate - after git struggle

--- generate.py
+++ generate.py
@@ -1,40 +1,29 @@
 from hashlib import sha256
-import torch
 from transformers import GPT2LMHeadModel
 from transformers import PreTrainedTokenizerFast
 
 
 class GenerateMidiText:
-    def __init__(self, model, tokenizer):
+    def __init__(self, model, tokenizer, device="cpu"):
         self.model = model
         self.tokenizer = tokenizer
+        self.device = device
+        # self.load_GPT2_model(model_path)
+        # self.load_tokenizer(tokenizer_path)
 
-    # Load the pretrained GPT2 model.
-    def load_GPT2_model(model_path, device="cpu"):
-        print("Loading model...")
-        model = GPT2LMHeadModel.from_pretrained(model_path).to(device)
-        return model
-
-    # Load the tokenizer.
-    def load_tokenizer(tokenizer_path, device="cpu"):
-        print("Loading tokenizer...")
-        tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
-        return tokenizer
-
-    # tokenize the prompt
-    def tokenize_prompt(prompt, verbose=True, device="cpu"):
-        prompt_ids = self.tokenizer.encode(prompt, return_tensors="pt")
-        if device == "cuda":  # TO CHECK - not sure if it works
-            prompt_ids.cuda()
+    def tokenize_input(self, input, verbose=False):
+        input_ids = self.tokenizer.encode(input, return_tensors="pt")
+        if self.device == "cuda":  # TO CHECK - not sure if it works
+            input_ids.cuda()
 
         if verbose:
-            print(f"inputs: {self.tokenizer.decode(prompt_ids[0])}")
-        return prompt_ids
+            print(f"inputs: {self.tokenizer.decode(input_ids[0])}")
+        return input_ids
 
-    # generate from the tokenized prompt
-    def generate_the_next_8_bars(prompt_ids, verbose=False):
+    # generate from the tokenized input
+    def generate_the_next_8_bars(self, input_ids, verbose=False):
         generated_ids = self.model.generate(
-            prompt_ids,
+            input_ids,
             max_length=2048,
             do_sample=True,
             temperature=0.75,
@@ -45,62 +34,92 @@ class GenerateMidiText:
         return generated_ids
 
     # convert the generated tokens to string
-    def convert_ids_to_text(generated_ids, verbose=True):
+    def convert_ids_to_text(self, generated_ids, verbose=False):
         generated_text = self.tokenizer.decode(generated_ids[0])
         if verbose:
             print(f"output: {generated_text}")
         return generated_text
 
-    def generate_one_sequence(prompt="PIECE_START", inst=None, density=None):
+    def generate_one_sequence(self, input="PIECE_START", inst=None, density=None):
         if inst is not None:  # ex: inst="INST=DRUMS"
-            prompt = f"{prompt} TRACK_START {inst} "
-        if density is not None:  # ex: inst="INST=DRUMS"
-            prompt = f"{prompt} TRACK_START {inst} DENSITY={density}"
+            input = f"{input} TRACK_START {inst} "
+            if density is not None:  # ex: inst="INST=DRUMS"
+                input = f"{input} DENSITY={density}"
+        if inst is None and density is not None:
+            print("Density cannot be defined without an input instrument #TOFIX")
 
-        input_ids = tokenize_prompt(prompt)
-        generated_ids = generate_the_next_8_bars(input_ids)
-        generated_text = convert_ids_to_text(generated_ids)
+        input_ids = self.tokenize_input(input)
+        generated_ids = self.generate_the_next_8_bars(input_ids)
+        generated_text = self.convert_ids_to_text(generated_ids)
         return generated_text
 
     def generate_multi_track_sequence(
-        model,
-        tokenizer,
+        self,
         inst_list=["INST=DRUMS", "INST=38", "INST=82"],
         density_list=[3, 6, 2],
     ):
         generated_multi_track_sequence = []
-        prompt = "PIECE_START"
+        input = "PIECE_START"
         for inst, density in zip(inst_list, density_list):
-            prompt = generate_one_sequence(
-                model, tokenizer, prompt=f"{prompt}", inst=inst, density=density
+            input = self.generate_one_sequence(
+                input=f"{input}", inst=inst, density=density
             )
-            generated_multi_track_sequence.append(prompt)
+        generated_multi_track_sequence = input
         return generated_multi_track_sequence
 
-    # utils saving to file
-    def hashing_seq(generated_sequence):
-        seq_cats = ""
-        for thisseq in generated_sequence:
-            seq_cats += thisseq[0]
-        filename = sha256(seq_cats.encode("utf-8")).hexdigest()
-        return filename
 
-    def writing_seq_to_file(generated_sequence, filepath_name):
-        file_object = open(f"{filepath_name}.txt", "w")
-        [file_object.writelines(f"{lines[0]} \n") for lines in generated_sequence]
+class WriteTextMidiToFile:  # utils saving to file
+    def __init__(self, sequence, output_path):
+        self.sequence = sequence
+        self.output_path = output_path
+
+    def hashing_seq(self):
+        self.filename = sha256(self.sequence.encode("utf-8")).hexdigest()
+        self.output_path_filename = f"{self.output_path}/{self.filename}.txt"
+
+    def writing_seq_to_file(self):
+        file_object = open(f"{self.output_path_filename}", "w")
+        assert type(self.sequence) is str, "sequence must be a string"
+        file_object.writelines(self.sequence)
         file_object.close()
+        print(f"Token sequence written: {self.output_path_filename}")
 
-        return print(f"Token sequence written: {filepath_name}.txt")
+    def text_midi_to_file(self):
+        self.hashing_seq()
+        self.writing_seq_to_file()
 
 
 if __name__ == "__main__":
 
     device = "cpu"
-    model_path = (
-        "/content/drive/MyDrive/the_jam_machine/model_jean_2048/checkpoint-50000/"
-    )
-    tokenizer_path = (
-        "/content/drive/MyDrive/the_jam_machine/model_jean_2048/tokenizer.json"
-    )
-    model = load_GPT2_model(model_path, device)
-    tokenizer = load_tokenizer(tokenizer_path, device)
+    # model path
+    model_path = "./models/model_2048_10kseq"
+    tokenizer_path = "./models/model_2048_10kseq/tokenizer.json"
+    generated_sequence_files_path = "models/model_2048_10kseq/generated_sequences/"
+
+    # load model and tokenizer
+    model = GPT2LMHeadModel.from_pretrained(model_path).to(device)
+    tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
+
+    # instantiate the GenerateMidiText class
+    gen = GenerateMidiText(model, tokenizer, device)
+    # generate a sequence
+    # generated_sequence = gen.generate_one_sequence(input="PIECE_START")
+    # WriteTextMidiToFile(
+    #     generated_sequence, generated_sequence_files_path
+    # ).text_midi_to_file()
+
+    # generate a DRUM sequence
+    # generated_sequence = gen.generate_one_sequence(inst="INST=DRUMS")
+    # WriteTextMidiToFile(
+    #     generated_sequence, generated_sequence_files_path
+    # ).text_midi_to_file()
+
+    # generate a multi track sequence
+    generated_multi_track_sequence = gen.generate_multi_track_sequence()
+
+    # write to file
+    WriteTextMidiToFile(
+        generated_multi_track_sequence, generated_sequence_files_path
+    ).text_midi_to_file()
+    generated_multi_track_sequence
