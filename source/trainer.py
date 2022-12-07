7be715c9754297ac83a3c22ee472e3574fd30504
m41w4r3.exe <33025255+loopbender@users.noreply.github.com>
2022-12-07 04:33:33 +0100

    update trainer with load_dataset from zip file

--- source/trainer.py
+++ source/trainer.py
@@ -19,93 +19,84 @@ from tokenizers.pre_tokenizers import WhitespaceSplit
 from tokenizers.trainers import WordLevelTrainer
 from datetime import datetime
 import wandb
+from datasets import load_dataset
 
 # CONFIG:
 TRAIN_FROM_CHECKPOINT = None  # Example: checkpoint-80000
 EVAL_STEPS = 1000
-PER_DEVICE_TRAIN_BATCH_SIZE = 10
+PER_DEVICE_TRAIN_BATCH_SIZE = 2
 TRAIN_EPOCHS = 5
 
+# Set paths either from Google Drive or locally
 formattedtime = datetime.now().strftime("%d-%m__%H-%M-%S")
-wandb.init(project=f"the-jam-machine-{formattedtime}")
-
 try:
     from google.colab import drive
 
+    wandb.init(project=f"the-jam-machine-{formattedtime}")
     drive.mount("/content/gdrive")
     drive_path = Path("/content/gdrive/MyDrive/the_jam_machine")
-    dataset_path = Path(f"{drive_path}/midi_encoded")
+    dataset_path = Path(f"{drive_path}/midi_encode")
     model_path = Path(f"{drive_path}/model_{formattedtime}")
 except:
     dataset_path = "./midi_encoded"
     model_path = f"./models/model_{formattedtime}"
-
+tokenizer_path = f"{model_path}/tokenizer.json"
 if not os.path.exists(model_path):
     os.mkdir(model_path)
 
-tokenizer_path = f"{model_path}/tokenizer.json"
-dataset_path_train = os.path.join(dataset_path, "train")
-dataset_path_valid = os.path.join(dataset_path, "validate")
+"""Load dataset from gzip files"""
+train_data = load_dataset(dataset_path, data_files={"train": "train/*.zip"})
+validate_data = load_dataset(dataset_path, data_files={"validate": "validate/*.zip"})
 
-
-def get_dataset(dataset_path):
-    all_files_paths = [f"{dataset_path}/{file}" for file in os.listdir(dataset_path)]
-    dataset = []
-    for file_path in all_files_paths:
-        with open(file_path) as file:
-            for line in file:
-                dataset.append(line.rstrip("\n"))
-    return dataset
-
-
-dataset_train = get_dataset(dataset_path_train)
-dataset_validate = get_dataset(dataset_path_valid)
-
-
-if TRAIN_FROM_CHECKPOINT is None:
+"""Get tokenizer from scratch or saved tokenizer.json"""
+if not os.path.isfile(tokenizer_path):
     tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
     tokenizer.pre_tokenizer = WhitespaceSplit()
     tokenizer_trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[MASK]"])
-    tokenizer.train_from_iterator(dataset_train, trainer=tokenizer_trainer)
+    tokenizer.train_from_iterator(
+        train_data["train"]["text"], trainer=tokenizer_trainer
+    )
     tokenizer.save(tokenizer_path)
 tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
 tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 print("Vocabulary size: ", tokenizer.vocab_size)
 
 
-def tokenize_function(data_to_tokenize):
-    tokenized_data = tokenizer(
-        data_to_tokenize,
+def tokenize(data):
+    return tokenizer(
+        data["text"],
         truncation=True,
         padding=True,
         max_length=2048,
     )
-    return {"input_ids": tokenized_data["input_ids"]}
 
 
-dataset_train_tokenized = list(map(tokenize_function, dataset_train))
-dataset_val_tokenized = list(map(tokenize_function, dataset_validate))
-
-
-assert list(dataset_train_tokenized[0]) == ["input_ids"], list(
-    dataset_train_tokenized[0]
+train_data_tokenized = train_data["train"].map(
+    tokenize, batched=True, remove_columns=["text"]
 )
-# Check a few samples
-for i, data in enumerate(dataset_train[:3]):
+validate_data_tokenized = validate_data["validate"].map(
+    tokenize, batched=True, remove_columns=["text"]
+)
+
+"""Make sure the tokenized dataset structure is correct and check a few examples"""
+assert "input_ids" in list(train_data_tokenized[0]), list(train_data_tokenized[0])
+for i, data in enumerate(train_data["train"]["text"][:3]):
     print("----")
     print(data)
-    print(dataset_train_tokenized[i])
+    print(train_data_tokenized[i]["input_ids"])
 
-model_config = GPT2Config(
-    vocab_size=tokenizer.vocab_size,
-    pad_token_id=tokenizer.pad_token_id,
-    n_embd=512,
-    n_head=8,
-    n_layer=10,
-    n_positions=2048,
+
+"""Create model and trainer"""
+model = GPT2LMHeadModel(
+    GPT2Config(
+        vocab_size=tokenizer.vocab_size,
+        pad_token_id=tokenizer.pad_token_id,
+        n_embd=512,
+        n_head=8,
+        n_layer=8,
+        n_positions=2048,
+    )
 )
-model = GPT2LMHeadModel(model_config)
-
 training_args = TrainingArguments(
     output_dir=model_path,
     overwrite_output_dir=True,
@@ -125,23 +116,23 @@ training_args = TrainingArguments(
     report_to="wandb",
     seed=42,
 )
-
 data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
 trainer = Trainer(
     model=model,
     args=training_args,
     data_collator=data_collator,
-    train_dataset=dataset_train_tokenized,
-    eval_dataset=dataset_val_tokenized,
+    train_dataset=train_data_tokenized,
+    eval_dataset=validate_data_tokenized,
 )
 
+"""Train the model from scratch or from checkpoint"""
 if TRAIN_FROM_CHECKPOINT is not None:
     trainer.train(f"{model_path}/{TRAIN_FROM_CHECKPOINT}")
 else:
     trainer.train()
 
 
-# Save the tokenizer.
+"""Save the tokenizer, latest status of trained model and push it to hugging face."""
 tokenizer.save_pretrained(model_path)
 model.save_pretrained(model_path)
-trainer.push_to_hub()
+trainer.push_to_hub(f"model_{formattedtime}")
