67baba3f876046017c80b8b6427a0abe3fe27f2e
Jean Simonnet <jeanasimonnet@gmail.com>
2022-12-10 23:53:31 +0100

    tidying up trainer.py

--- source/trainer.py
+++ source/trainer.py
@@ -13,97 +13,57 @@
 """
 
 import os
-from pathlib import Path
 from transformers import (
-    PreTrainedTokenizerFast,
     DataCollatorForLanguageModeling,
     GPT2Config,
     GPT2LMHeadModel,
     TrainingArguments,
     Trainer,
 )
-from tokenizers import Tokenizer
-from tokenizers.models import WordLevel
-from tokenizers.pre_tokenizers import WhitespaceSplit
-from tokenizers.trainers import WordLevelTrainer
-from datetime import datetime
 import wandb
 from datasets import load_dataset
-from pynvml import *
-from trainer_utils import get_history, plot_history
 
+# from pynvml import *
+from trainer_utils import *
 
 # CONFIG:
-""" 
-The Checkpoint path folder will here be in a distinct folder
-Because a new name is given for every model using formattedtime
-"""
+
+# The Checkpoint path folder will here be in a distinct folder
+# Because a new name is given for every model using formattedtime
 TRAIN_FROM_CHECKPOINT = None  # Example: fullpath/model/checkpoint-80000
 ADDITIONAL_TRAIN_EPOCHS = 0  # only used if TRAIN_FROM_CHECKPOINT is not None
 EVAL_STEPS = 1000
-PER_DEVICE_TRAIN_BATCH_SIZE = 4
-GRADIENT_ACCUMULATION_STEPS = 2
+PER_DEVICE_TRAIN_BATCH_SIZE = 2
+GRADIENT_ACCUMULATION_STEPS = 3
 TRAIN_EPOCHS = 5
+# MODEL_RUN_IN = "gdrive"
+MODEL_RUN_IN = "local"
+# wandb.init(project=f"the-jammy-machine")
+HF_REPO = "misnaej/the-jam-machine"
 
-"""Set paths either from Google Drive or locally"""
-formattedtime = datetime.now().strftime("%d-%m__%H-%M-%S")
-try:
-    from google.colab import drive
-
-    wandb.init(project=f"the-jammy-machine")
-    drive.mount("/content/gdrive")
-    drive_path = "/content/gdrive/MyDrive/the_jam_machine"
-    dataset_path = f"{drive_path}/midi_encoded"
-    model_path = f"{drive_path}/model_{formattedtime}"
-    MODEL_RUN_IN = "gdrive"
-except:
-    dataset_path = "./midi_encoded"
-    model_path = f"./models/model_{formattedtime}"
-    MODEL_RUN_IN = "local"
-    HF_REPO = "misnaej/the-jam-machine"
-tokenizer_path = f"{model_path}/tokenizer.json"
-
-if not os.path.exists(model_path):
-    os.mkdir(model_path)
+"""set paths """
+base_path = "/Users/jean/WORK/DSR_2022_b32/music_portfolio/the_jam_machine_github/the-jam-machine"
+model_path = "models/model_elec_familiarised"
+data_path = "midi/dataset/electronic/familiarised"
+paths = set_paths(base_path, model_path, data_path, model_run_in=MODEL_RUN_IN)
+tokenizer_path = paths["tokenizer_path"]
+dataset_path = paths["dataset_path"]
+model_path = paths["model_path"]
 
 """Load dataset from gzip files"""
 train_data = load_dataset(dataset_path, data_files={"train": "train/*.zip"})["train"]
 validate_data = load_dataset(dataset_path, data_files={"val": "validate/*.zip"})["val"]
 
-# TODO: Move tokenizer logic to encoder and use its json here only.
 """Get tokenizer from scratch or saved tokenizer.json"""
-if not os.path.isfile(tokenizer_path):
-    tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
-    tokenizer.pre_tokenizer = WhitespaceSplit()
-    tokenizer_trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[MASK]"])
-    tokenizer.train_from_iterator(train_data["text"], trainer=tokenizer_trainer)
-    tokenizer.save(tokenizer_path)
-tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
-tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-print("Vocabulary size: ", tokenizer.vocab_size)
+tokenizer = train_tokenizer(tokenizer_path, train_data)
 
+print("=======Tokenizing Train dataset========")
+train_data_tokenized = TokenizeDataset(tokenizer).batch_tokenization(train_data)
+check_tokenized_data(train_data, train_data_tokenized)
 
-def tokenize(data):
-    return tokenizer(
-        data["text"],
-        truncation=True,
-        padding=True,
-        max_length=2048,
-    )
-
-
-train_data_tokenized = train_data.map(tokenize, batched=True, remove_columns=["text"])
-validate_data_tokenized = validate_data.map(
-    tokenize, batched=True, remove_columns=["text"]
-)
-
-"""Make sure the tokenized dataset structure is correct and check a few examples"""
-assert "input_ids" in list(train_data_tokenized[0]), list(train_data_tokenized[0])
-for i, data in enumerate(train_data["text"][:3]):
-    print("----")
-    print(data)
-    print(train_data_tokenized[i]["input_ids"])
-
+print("=======Tokenizing Validation dataset========")
+validate_data_tokenized = TokenizeDataset(tokenizer).batch_tokenization(validate_data)
+check_tokenized_data(validate_data, validate_data_tokenized)
 
 """Create model and trainer"""
 model = GPT2LMHeadModel(
@@ -128,7 +88,7 @@ training_args = TrainingArguments(
     lr_scheduler_type="cosine",
     per_device_train_batch_size=PER_DEVICE_TRAIN_BATCH_SIZE,
     gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
-    fp16=True,
+    fp16=False,
     save_strategy="steps",
     save_steps=EVAL_STEPS * 4,
     save_total_limit=5,
--- source/trainer_utils.py
+++ source/trainer_utils.py
@@ -1,5 +1,87 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from pathlib import Path
+from datetime import datetime
+
+from transformers import PreTrainedTokenizerFast
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.pre_tokenizers import WhitespaceSplit
+from tokenizers.trainers import WordLevelTrainer
+
+
+class TokenizeDataset:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def tokenize(self, data):
+        return self.tokenizer(
+            data["text"],
+            truncation=True,
+            padding=True,
+            max_length=2048,
+        )
+
+    def batch_tokenization(self, dataset):
+        dataset_tokenized = dataset.map(
+            self.tokenize, batched=True, remove_columns=["text"]
+        )
+        return dataset_tokenized
+
+
+def train_tokenizer(tokenizer_path, train_data, verbose=True):
+    if not os.path.isfile(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = WhitespaceSplit()
+        tokenizer_trainer = WordLevelTrainer(
+            special_tokens=["[UNK]", "[PAD]", "[MASK]"]
+        )
+        tokenizer.train_from_iterator(train_data["text"], trainer=tokenizer_trainer)
+        tokenizer.save(tokenizer_path)
+
+    tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
+    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+    print("Vocabulary size: ", tokenizer.vocab_size)
+    if verbose:
+        print("Vocabulary:")
+        [print(voc) for voc in sorted(tokenizer.vocab.items())]
+
+    return tokenizer
+
+
+def check_tokenized_data(dataset, tokenized_dataset):
+    assert "input_ids" in list(tokenized_dataset[0]), list(tokenized_dataset[0])
+    for i, data in enumerate(dataset["text"][:100:20]):
+        print("----")
+        print(data)
+        print(tokenized_dataset[i]["input_ids"])
+
+
+def set_paths(base_path, model_name, datafolder, model_run_in="local"):
+    """Set paths either from Google Drive or locally"""
+    # formattedtime = datetime.now().strftime("%d-%m__%H-%M-%S")
+    formattedtime = "10-12__23-08-42"
+    if model_run_in == "gdrive":
+        from google.colab import drive
+
+        drive.mount("/content/gdrive")
+        base_path = f"/content/gdrive/MyDrive/{base_path}"
+    elif model_run_in == "local":
+        pass
+
+    dataset_path = f"{base_path}/{datafolder}"
+    model_path = f"{base_path}/{model_name}_{formattedtime}"
+    tokenizer_path = f"{model_path}/tokenizer.json"
+    if not os.path.exists(model_path):
+        print(f"Creating model path: {model_path}")
+        os.mkdir(model_path)
+
+    return {
+        "tokenizer_path": tokenizer_path,
+        "dataset_path": dataset_path,
+        "model_path": model_path,
+    }
 
 
 def get_history(trainer):
@@ -19,7 +101,6 @@ def get_history(trainer):
         "valid_epoch": np.array(valid_history).T[0],
         "valid_loss": np.array(valid_history).T[2],
     }
-
     return history
 
 
