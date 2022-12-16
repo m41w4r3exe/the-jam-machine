5ae39d4895c9c9222de3c3df4f134a742360c348
Jean Simonnet <jeanasimonnet@gmail.com>
2022-12-16 17:42:07 +0100

    trying piano roll

--- source/playground.py
+++ source/playground.py
@@ -6,6 +6,7 @@ from encoder import MIDIEncoder
 from decoder import TextDecoder
 from utils import get_miditok
 from playback import get_music
+import matplotlib.pyplot as plt
 
 model_repo = "JammyMachina/elec-gmusic-familized-model-13-12__17-35-53"
 model, tokenizer = LoadModel(
@@ -20,7 +21,7 @@ decoder = TextDecoder(miditok)
 
 
 def generator(temp, density, instrument, state):
-    genesis.set_temperature(temp)
+    genesis.set_temperatures([temp])
 
     inst = next(
         (inst for inst in INSTRUMENT_CLASSES if inst["name"] == instrument),
@@ -34,9 +35,26 @@ def generator(temp, density, instrument, state):
         generated_text, "temporary.mid"
     )  # Returns miditoolkit, not compatible with pretty_midi
     p_midi, audio = get_music("temporary.mid")
+    note_list = p_midi.instruments[0].notes
     state = generated_text
     return generated_text, (44100, audio), state
 
+
+""" Plot Piano Roll """
+
+
+def plot_piano_roll(p_midi_note_list):
+    for note in p_midi_note_list:
+        plt.plot(
+            [note.start, note.end], [note.pitch, note.pitch], color="k", linewidth=3
+        )
+    plt.xlabel("ticks")
+    plt.show()
+    plt.close()
+
+
+# output_txt, audio, state, note_list = generator(0.75, 2, "Drums", "PIECE_START")
+# plot_piano_roll(note_list)
 
 with gr.Blocks() as demo:
     state = gr.State("PIECE_START")
@@ -51,15 +69,19 @@ with gr.Blocks() as demo:
                 temp = gr.Number(value=0.75, label="Temperature")
                 density = gr.Dropdown([0, 1, 2, 3], value=2, label="Density")
         with gr.Column(scale=1, min_width=100):
+            notes = gr.Interface(plot_piano_roll)
             output_txt = gr.Textbox(label="output", lines=6, max_lines=6)
+
         with gr.Column(scale=1, min_width=100):
             audio = gr.Audio(label="Audio")
             gen_btn = gr.Button("Generate")
             gen_btn.click(
                 fn=generator,
                 inputs=[temp, density, inst, state],
-                outputs=[output_txt, audio, state],
+                outputs=[output_txt, audio, state, notes],
             )
+
+    ## INPUT PROMPT
     with gr.Row():
         with gr.Column(scale=1, min_width=10):
             gr.Markdown(
@@ -89,3 +111,4 @@ with gr.Blocks() as demo:
             # greet_btn.click(fn=greet, inputs=name, outputs=output)
 
 demo.launch()
+print("bye")
