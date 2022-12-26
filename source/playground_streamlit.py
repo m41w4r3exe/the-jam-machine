a0eb96990eb044da36efed0d4aac917debc3a6ba
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2022-12-26 08:16:46 +0100

    App layout almost done

--- source/playground_streamlit.py
+++ source/playground_streamlit.py
@@ -12,6 +12,15 @@ import matplotlib
 from generation_utils import plot_piano_roll
 import numpy as np
 import streamlit as st
+from familizer import Familizer
+
+# TODO: Fix caching issues
+# TODO: Fix generate correct instrument
+# TODO: simplify state and piece_by_track to just one variable
+# TODO: remove track_index and work with track names instead
+# TODO: remove regenerate state
+# TODO: add a button to export the generated midi
+# TODO: add a button to clear the state
 
 matplotlib.use("Agg")
 sys.modules["pylab"] = pylab
@@ -29,13 +38,13 @@ miditok = get_miditok()
 decoder = TextDecoder(miditok)
 
 
-def define_prompt(state, genesis):
-    """If somethikng has already been generated, use this as a prompt,
+def get_prompt(state, composer):
+    """If something has already been generated, use this as a prompt,
     else use PIECE_START"""
     if len(state) == 0:
         input_prompt = "PIECE_START "
     else:
-        input_prompt = genesis.get_whole_piece_from_bar_dict()
+        input_prompt = composer.get_whole_piece_from_bar_dict()
     return input_prompt
 
 
@@ -45,66 +54,59 @@ def generator(
     temp,
     density,
     instrument,
-    state,
+    state,  # list of generated tracks
     piece_by_track,  # dictionary of generated tracks
-    add_bars=False,
-    add_bar_count=1,
 ):
     # initialize composer with model and generated tracks
     composer = GenerateMidiText(model, tokenizer, piece_by_track)
-    track = {"label": label}
-    inst = next(
-        (inst for inst in INSTRUMENT_CLASSES if inst["transfer_to"] == instrument),
-        {"family_number": "DRUMS"},
-    )["family_number"]
+    new_track = {"label": label}
+    instrument_family = Familizer().get_family_number(instrument)
 
-    inst_index = -1  # default to last generated
+    # Check if instrument already exists
+    track_index = -1  # default to last generated
     if state != []:
-        for index, instrum in enumerate(state):
-            if instrum["label"] == track["label"]:
-                inst_index = index  # changing if exists
+        for i, current_track in enumerate(state):
+            if current_track["label"] == new_track["label"]:
+                track_index = i  # changing if exists
 
-    # Generate
-    if not add_bars:
-        # Regenerate
-        if regenerate:
-            state.pop(inst_index)
-            composer.delete_one_track(inst_index)
+    # If instrument track already exists, delete it
+    if regenerate:
+        # Delete track
+        state.pop(track_index)  # redundant
+        composer.delete_one_track(track_index)
+        track_index = -1  # reset to last generated
 
-            generated_text = (
-                composer.get_whole_piece_from_bar_dict()
-            )  # maybe not useful here
-            inst_index = -1  # reset to last generated
+    # Generate new track
+    input_prompt = get_prompt(state, composer)
+    generated_text = composer.generate_one_new_track(
+        instrument_family, density, temp, input_prompt=input_prompt
+    )
 
-        # NEW TRACK
-        input_prompt = define_prompt(state, composer)
-        generated_text = composer.generate_one_new_track(
-            inst, density, temp, input_prompt=input_prompt
-        )
+    regenerate = True  # set regenerate to true
 
-        regenerate = True  # set generate to true
-    else:
-        # NEW BARS
-        composer.generate_n_more_bars(add_bar_count)  # for all instruments
-        generated_text = composer.get_whole_piece_from_bar_dict()
-
+    # convert generated text to midi and audio and save files locally
     decoder.get_midi(generated_text, "mixed.mid")
     mixed_inst_midi, mixed_audio = get_music("mixed.mid")
 
-    inst_text = composer.get_selected_track_as_text(inst_index)
+    # get last generated track and save it locally
+    new_track_text = composer.get_selected_track_as_text(track_index)
     inst_midi_name = f"{instrument}.mid"
-    decoder.get_midi(inst_text, inst_midi_name)
+    decoder.get_midi(new_track_text, inst_midi_name)
     _, inst_audio = get_music(inst_midi_name)
+
+    # get piano roll figure
     piano_roll = plot_piano_roll(mixed_inst_midi)
-    track["text"] = inst_text
-    state.append(track)
+
+    # update state with new track
+    new_track["text"] = new_track_text
+    state.append(new_track)
 
     return (
-        inst_text,
-        (44100, inst_audio),
+        new_track_text,
+        inst_audio,
         piano_roll,
         state,
-        (44100, mixed_audio),
+        mixed_audio,
         regenerate,
         composer.piece_by_track,
     )
@@ -125,7 +127,7 @@ def instrument_row(default_inst, row_id, col):
         min_value=0.1,
         max_value=1.0,
         value=0.7,
-        step=0.1,
+        step=0.05,
         key=row_id + 2,
     )
 
@@ -151,23 +153,36 @@ def instrument_row(default_inst, row_id, col):
             regenerate,
             piece_by_track,
         ) = generator(
-            row_id, regenerate, temp, density, instrument, state, piece_by_track
+            row_id,
+            st.session_state["regenerate"],
+            temp,
+            density,
+            instrument,
+            st.session_state["state"],
+            st.session_state["piece_by_track"],
         )
 
         # display generated track audio
         col.subheader("Track Audio")
-        col.audio(inst_audio)
+        col.audio(inst_audio, sample_rate=44100)
+
+        # display mixed audio
+        st.subheader("Mixed Audio")
+        st.audio(mixed_audio, sample_rate=44100)
 
         # display generated piano roll
-        col.subheader("Piano Roll")
-        col.pyplot(piano_roll)
+        st.subheader("Piano Roll")
+        st.pyplot(piano_roll)
 
 
 def main():
     # initialize state variables
-    piece_by_track = {}
-    state = []
-    regenerate = False
+    if "piece_by_track" not in st.session_state:
+        st.session_state.piece_by_track = []
+    if "state" not in st.session_state:
+        st.session_state.state = []
+    if "regenerate" not in st.session_state:
+        st.session_state.regenerate = False
 
     # setup page layout
     st.set_page_config(layout="wide")
